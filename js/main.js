520f244263175c9c69ae9da7d738598394d99e8c
TinhDk <jackymon41@gmail.com>
2018-07-18 08:18:57 +0700

    update source cv

--- js/main.js
+++ js/main.js
@@ -230,5 +230,30 @@
     $('.site-main-menu').on("click", "a", function (e) {
         mobileMenuHide();
     });
+    $('.tabs-resume').click(function() {
 
+        $('.skillbar').each(function() {
+            $(this).find('.skillbar-bar').width(0);
+        });
+
+        $('.skillbar').each(function() {
+            $(this).find('.skillbar-bar').animate({
+                width: $(this).attr('data-percent')
+            }, 2000);
+        });
+
+    });
+
+    $('#resume').prev('h2.resp-accordion').click(function() {
+
+        $('.skillbar').each(function() {
+            $(this).find('.skillbar-bar').width(0);
+        });
+
+        $('.skillbar').each(function() {
+            $(this).find('.skillbar-bar').animate({
+                width: $(this).attr('data-percent')
+            }, 2000);
+        });
+    });
 })(jQuery);
